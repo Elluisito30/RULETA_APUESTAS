b8d46b9c95d748d6628e3d74dbade743ecaed584
Dalessandro Zahit Moreno Aguilar <dzmorenoag@unitru.edu.pe>
2024-12-11 13:39:06 -0500

    modificacion2

--- src/entidades/Ficha.java
+++ src/entidades/Ficha.java
@@ -1,6 +1,32 @@
-
 package entidades;
 
 public class Ficha {
-    
+    private int valor;
+    private String color; // Opcional: si las fichas tienen color
+
+    public Ficha(int valor, String color) {
+        this.valor = valor;
+        this.color = color;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "Ficha {valor=" + valor + ", color='" + color + "'}";
+    }
 }
